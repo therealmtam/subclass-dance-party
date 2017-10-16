9dc069fac70e533948937f67583276dd04f1158b
Hack Reactor Student <student@hackreactor.com>
2017-10-16 16:07:32 -0700

    implemented psuedoclassical style for blinkydancer.js

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -17,3 +17,16 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
 
   return blinkyDancer;
 };
+
+var BlinkyDancer = function (top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+  var oldStep = this.step;
+};
+
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+BlinkyDancer.prototype.constructor = BlinkyDancer;
+BlinkyDancer.prototype.step = function () {
+  Dancer.prototype.step.call(this);
+  
+  this.$node.toggle();
+};

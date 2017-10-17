0bc97324b26f9f3cb7b2cfcaeb6f8be95c44efe1
Hack Reactor Student <student@hackreactor.com>
2017-10-16 17:15:01 -0700

    fixed error with pseudoclassical method of step

--- src/dancer.js
+++ src/dancer.js
@@ -43,7 +43,8 @@ var Dancer = function (top, left, timeBetweenSteps) {
 
 //Dancer Methods
 Dancer.prototype.step = function (timeBetweenSteps) {
-  setTimeout(this.step, timeBetweenSteps);
+  setTimeout(this.step.bind(this, timeBetweenSteps), timeBetweenSteps);
+
 };
 
 Dancer.prototype.setPosition = function (top, left) {
